1b85ad983eee52d623cde3e421c7cfb33993a368
Hassan Al Jammal <hassan-jammal@hotmail.com>
2026-01-10 04:22:46 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,49 +12,49 @@ export default defineNuxtConfig({
 			htmlAttrs: {
 				lang: "en",
 			},
-			// link: [
-			// 	{
-			// 		rel: "icon",
-			// 		type: "image/png",
-			// 		href: "https://eaula.vercel.app/images/favicon-64x64.png",
-			// 	},
-			// 	{
-			// 		rel: "icon",
-			// 		type: "image/png",
-			// 		href: "https://eaula.vercel.app/images/favicon-32x32.png",
-			// 	},
-			// 	{
-			// 		rel: "icon",
-			// 		type: "image/png",
-			// 		href: "https://eaula.vercel.app/images/favicon-16x16.png",
-			// 	},
-			// 	{
-			// 		rel: "apple-touch-icon",
-			// 		type: "image/png",
-			// 		href: "https://eaula.vercel.app/images/apple-touch-icon-180x180.png",
-			// 	},
-			// 	{
-			// 		rel: "mask-icon",
-			// 		href: "https://eaula.vercel.app/images/icons/mask-icon.svg",
-			// 		color: "#32393C",
-			// 	},
-			// 	{
-			// 		rel: "icon",
-			// 		sizes: "192x192",
-			// 		href: "https://eaula.vercel.app/images/android-chrome-192x192.png",
-			// 	},
-			// 	{
-			// 		rel: "icon",
-			// 		sizes: "512x512",
-			// 		href: "https://eaula.vercel.app/images/android-chrome-512x512.png",
-			// 	},
-			// ],
+			link: [
+				{
+					rel: "icon",
+					type: "image/png",
+					href: "https://https://creditcard-webapp.vercel.app/images/favicon-64x64.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					href: "https://https://creditcard-webapp.vercel.app/images/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					href: "https://https://creditcard-webapp.vercel.app/images/favicon-16x16.png",
+				},
+				{
+					rel: "apple-touch-icon",
+					type: "image/png",
+					href: "https://https://creditcard-webapp.vercel.app/images/apple-touch-icon-180x180.png",
+				},
+				{
+					rel: "mask-icon",
+					href: "https://https://creditcard-webapp.vercel.app/images/mask-icon.svg",
+					color: "#000000",
+				},
+				{
+					rel: "icon",
+					sizes: "192x192",
+					href: "https://https://creditcard-webapp.vercel.app/images/android-chrome-192x192.png",
+				},
+				{
+					rel: "icon",
+					sizes: "512x512",
+					href: "https://https://creditcard-webapp.vercel.app/images/android-chrome-512x512.png",
+				},
+			],
 			meta: [
-				{ name: "theme-color", content: "#AEB4BD" },
+				{ name: "theme-color", content: "#000000" },
 				{ name: "format-detection", content: "telephone=no" },
-				{ name: "author", content: "Comium" },
-				{ name: "og:site_name", content: "Comium" },
-				{ name: "og:image:alt", content: "Comium" },
+				{ name: "author", content: "MyMonty Credit Card Web Application" },
+				{ name: "og:site_name", content: "MyMonty Credit Card Web Application" },
+				{ name: "og:image:alt", content: "MyMonty Credit Card Web Application" },
 				{ name: "og:image:width", content: "1200" },
 				{ name: "og:image:height", content: "630" },
 				{ name: "mobile-web-app-capable", content: "yes" },

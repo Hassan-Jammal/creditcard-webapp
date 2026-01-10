c9bbfe954691b44b400ec5d47350bdc3c388542c
Hassan Al Jammal <hassan-jammal@hotmail.com>
2026-01-10 04:29:14 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
 		head: {
 			charset: "utf-8",
 			viewport: "width=device-width, initial-scale=1",
-			titleTemplate: "%sCredit Card Webp Application",
+			titleTemplate: "%sMyMonty Credit Card Web Application",
 			htmlAttrs: {
 				lang: "en",
 			},

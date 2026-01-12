5bd6957acc5b047087c3685c47bed4568bb134aa
Hassan Al Jammal <hassan-jammal@hotmail.com>
2026-01-12 21:59:37 +0200

    .

--- app/components/steps/StepSupportingInformation.vue
+++ app/components/steps/StepSupportingInformation.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="w-full space-y-8">
 		<FileUpload label="Valid ID or Passport" name="id_or_passport"
-			description="Documents format should be in JPEG or PDF" :maxFiles="3" :maxSizeMb="2"
+			description="Documents format should be in JPEG or PDF" :maxFiles="3" :maxSizeMb="4"
 			accept="pdf,doc,docx,jpg,jpeg,png" v-model="form.supporting_information_id_or_passport" />
 
 		<FileUpload label="Proof of Residence" name="proof_of_residence"
-			description="Documents format should be in JPEG or PDF" :maxFiles="2" :maxSizeMb="2"
+			description="Documents format should be in JPEG or PDF" :maxFiles="2" :maxSizeMb="4"
 			accept="pdf,jpg,jpeg,png" v-model="form.supporting_information_proof_of_residence" />
 
 		<FileUpload label="Bank Statement" name="bank_statement" description="Documents format should be in JPEG or PDF"
-			:maxFiles="2" :maxSizeMb="2" accept="pdf,jpg,jpeg,png"
+			:maxFiles="2" :maxSizeMb="4" accept="pdf,jpg,jpeg,png"
 			v-model="form.supporting_information_bank_statement" />
 
 		<FileUpload label="Employment Letter" name="employment_letter"
